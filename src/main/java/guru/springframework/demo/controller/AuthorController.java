9bf647e38667a4e59a9dcbcd45e001516ab4c322
Trần Nhật Thy <73516408+nhatthy24@users.noreply.github.com>
2021-10-31 19:59:46 +0700

    add controller, list books and list authors

--- src/main/java/guru/springframework/demo/controller/AuthorController.java
+++ src/main/java/guru/springframework/demo/controller/AuthorController.java
@@ -1,2 +1,21 @@
-package guru.springframework.demo.controller;public class AuthorController {
+package guru.springframework.demo.controller;
+
+import guru.springframework.demo.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors",authorRepository.findAll());
+        return "authors/list";
+    }
 }
--- src/main/java/guru/springframework/demo/controller/BookController.java
+++ src/main/java/guru/springframework/demo/controller/BookController.java
@@ -1,2 +1,25 @@
-package guru.springframework.demo.controller;public class BookController {
+package guru.springframework.demo.controller;
+
+import guru.springframework.demo.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books/list";
+    }
 }
